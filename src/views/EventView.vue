4ea329a146633c3925bf4474bec00146e168dcdb
ThomasGrare <thomasgrare64@gmail.com>
2023-11-28 22:58:38 +0100

    store pinia & fetch event

--- src/views/EventView.vue
+++ src/views/EventView.vue
@@ -1,56 +1,26 @@
 <template>
-    <div>
-      <h1>Events</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Events id</th>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Date</th>
-            <th>Place</th>
-            <th>Event Type</th>
-            <th>Organizer Id</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="evenements in evenements" :key="evenements.event_id">
-            <td>{{ evenements.event_id }}</td>
-            <td>{{ evenements.titre }}</td>
-            <td>{{ evenements.description }}</td>
-            <td>{{ evenements.date }}</td>
-            <td>{{ evenements.lieu }}</td>
-            <td>{{ evenements.type_evenement }}</td>
-            <td>{{ evenements.organisateur_id }}</td>
-            
-            
-          </tr>
-        </tbody>
-      </table>
-     
-    </div>
-  </template>
+  <div>
+    <h1>Events</h1>
+    <button @click="syncEvent()">test</button>
+
+    {{ evenements }}
+  </div>
+</template>
+
+  
 <script setup>
-import { supabase } from "../utils/supabase";
+import { useDataEventStore } from "../stores/dataEvent";
 import { ref } from "vue";
+
+const store = useDataEventStore();
 const evenements = ref([]);
 
-async function fetchData() {
-  try {
-    const { data, error } = await supabase
-      .from("evenements")
-      .select("*");
-    if (error) {
-      throw error;
-    }
+const syncEvent = async () => {
+  store.loadEventData();
+  evenements.value = store.events;
+};
 
-    evenements.value = data;
-  } catch (error) {
-    console.error(error);
-  }
-}
 
-fetchData();
 
 
 </script>
