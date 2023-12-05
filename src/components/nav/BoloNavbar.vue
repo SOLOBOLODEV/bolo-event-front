ba1dd248508592ead0d4d472222391d678bf2e90
HydreFunky11 <92850354+HydreFunky11@users.noreply.github.com>
2023-12-05 16:21:23 +0100

    Revert "nav responsive + com dans la popup des nos events"

--- src/components/nav/BoloNavbar.vue
+++ src/components/nav/BoloNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar flex top-0 w-full bg-white border-b border-gray-200 relative z-10">
+  <nav class="navbar flex top-0 w-full bg-white border-b border-gray-200">
     <div class="container mx-auto flex items-center justify-between p-4">
       <div class="flex items-center space-x-4">
         <a class="navbar-brand">
@@ -13,32 +13,14 @@
           <RouterLink to="">Swag</RouterLink>
           <button @click="logout()"> Déconnexion </button>
         </div>
-        <!-- Ajout d'un bouton pour le menu sur les écrans de taille inférieure à md -->
-        <div class="md:hidden">
-          <button @click="toggleMobileMenu" class="text-gray-500 hover:text-gray-700">
-            <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-            </svg>
-          </button>
-        </div>
-      </div>
-      <!-- Menu mobile -->
-      <div v-if="showMobileMenu" class="md:hidden absolute top-16 right-0 bg-white border border-gray-200 p-4 space-y-2">
-        <RouterLink to="/dashboard" @click="toggleMobileMenu">Accueil</RouterLink>
-        <RouterLink to="/events" @click="toggleMobileMenu">Évenement</RouterLink>
-        <RouterLink to="" @click="toggleMobileMenu">Swag</RouterLink>
-        <button @click="logout(); toggleMobileMenu()"> Déconnexion </button>
       </div>
     </div>
   </nav>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { RouterLink } from "vue-router";
 import { supabase } from "../../utils/supabase";
 
-const showMobileMenu = ref(false);
 
 const logout = async () => {
   try {
@@ -49,7 +31,4 @@ const logout = async () => {
   }
 };
 
-const toggleMobileMenu = () => {
-  showMobileMenu.value = !showMobileMenu.value;
-};
 </script>
