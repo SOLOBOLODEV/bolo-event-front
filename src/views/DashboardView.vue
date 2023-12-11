28d02545b31742f0b92ff7ba91e4ef835620af4c
Rémy <remy.berjonneau@gmail.com>
2023-12-11 09:10:20 +0100

    normalement c'est réparé

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -5,15 +5,18 @@
       v-if="showPopup">
       <div
         style="position: relative; width: 80%; aspect-ratio: 16/9; background-color: hsl(0, 0%, 80%); border-radius: 16px; box-shadow: 5px 5px 20px hsl(0, 0%, 20%);">
-        <div v-if="event.event_id == eventId" class="w-full mx-auto py-16">
-          <input type="text" v-model="event.titre" class="mb-4 p-2 border border-gray-300 block w-1/2 bg-teal-500" />
-          <input type="text" v-model="event.type_evenement"
-            class="mb-4 p-2 border border-gray-300 block w-1/2 bg-orange-600" />
-          <input type="text" v-model="event.lieu" class="mb-4 p-2 border border-gray-300 block w-1/2 bg-red-500" />
-          <input type="date" v-model="event.date" class="mb-4 p-2 border border-gray-300 block w-1/2 bg-green-500" />
-          <textarea v-model="event.description" class="mb-4 p-2 border border-gray-300 block w-1/2 h-60 bg-fuchsia-600" />
-          <button class="bg-red-500 rounded-sm p-4" @click="togglePopup(event.event_id)">Quitter</button>
-          <button class="bg-green-600 rounded-sm p-4 ml-32" @click="updateEvent(event)">Sauvegarder</button>
+        <div v-for="event in myEvents" :key="event">
+          <div v-if="event.event_id == eventId" class="w-full mx-auto py-16">
+            <input type="text" v-model="event.titre" class="mb-4 p-2 border border-gray-300 block w-1/2 bg-teal-500" />
+            <input type="text" v-model="event.type_evenement"
+              class="mb-4 p-2 border border-gray-300 block w-1/2 bg-orange-600" />
+            <input type="text" v-model="event.lieu" class="mb-4 p-2 border border-gray-300 block w-1/2 bg-red-500" />
+            <input type="date" v-model="event.date" class="mb-4 p-2 border border-gray-300 block w-1/2 bg-green-500" />
+            <textarea v-model="event.description"
+              class="mb-4 p-2 border border-gray-300 block w-1/2 h-60 bg-fuchsia-600" />
+            <button class="bg-red-500 rounded-sm p-4" @click="togglePopup(event.event_id)">Quitter</button>
+            <button class="bg-green-600 rounded-sm p-4 ml-32" @click="updateEvent(event)">Sauvegarder</button>
+          </div>
         </div>
       </div>
     </div>
@@ -28,20 +31,28 @@
             <th>
               En savoir plus
             </th>
-            <th></th>
+            <th>
+
+            </th>
           </tr>
         </thead>
         <tbody>
+          <!-- Séparer les 2 lignes en composants différents -->
           <tr v-for="event in myEvents" :key="event" class="odd:bg-gray-200 even:bg-gray-50 border-b">
             <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{{ event.titre != null ?
               event.titre : "non précisé" }}</td>
             <td class="px-6 py-4 text-gray-800">{{ event.lieu != null ? event.lieu : "non précisé" }}</td>
-            <td><button @click="togglePopup(event.event_id)">Plus</button></td>
+            <td class="px-6 py-4 text-gray-800">{{ event.date != null ? event.date : "non précisé" }}</td>
+            <td class="px-6 py-4 text-gray-800">{{ event.type_evenement != null ? event.type_evenement : "non précisé" }}
+            </td>
+            <td><button @click="togglePopup(event.event_id)">Cliquez</button></td>
             <td><button @click="deleteEvent(event.event_id)">Supprimer</button></td>
+            <!-- {{ event }} -->
           </tr>
         </tbody>
       </table>
     </div>
+
   </div>
 </template>
 
@@ -64,6 +75,8 @@ const userId = userSession.getUserId;
 const columns = [
   { name: "titre", label: "Vos Evennements" },
   { name: "lieu", label: "Lieu" },
+  { name: "date", label: "Date" },
+  { name: "type_evenement", label: "Type" }
 ];
 
 const togglePopup = (toggledEventId) => {
@@ -82,23 +95,7 @@ const displayMyEvent = async () => {
   myEvents.value = dataEvent.events;
   const eventList = myEvents.value.filter(event => event.organisateur_id === userId);
   myEvents.value = eventList;
-
-  // Parcourir tous les événements pour récupérer les commentaires
-  for (const event of myEvents.value) {
-    const { data, error } = await supabase
-      .from("feedbacks")
-      .select("commentaire")
-      .eq("event_id", event.event_id);
-
-    if (error) {
-      console.error("Erreur lors de la récupération des commentaires depuis Supabase:", error);
-    } else {
-      event.comments = data;
-    }
-  }
 };
-
-displayMyEvent();
 
 const deleteEvent = async (id) => {
   const userId = await userSession.getUserId;
@@ -116,6 +113,8 @@ const updateEvent = async (event) => {
 
 displayMyEvent();
 
+
+
 const logout = async () => {
   try {
     await supabase.auth.signOut();
@@ -123,4 +122,5 @@ const logout = async () => {
     console.error(error);
   }
 };
+
 </script>
