0dfe0a731e4cae4d5a69352289c603dc3a14035c
ThomasGrare <thomasgrare64@gmail.com>
2023-12-12 14:13:18 +0100

    fix loading infinite app

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,13 @@
 <template>
-  <BoloConnexion />
+  <BoloConnexion v-if="!userSession.session" />
 </template>
 
 
 <script setup>
 import BoloConnexion from "../components/connexion/BoloConnexion.vue";
+import { userSessionStore } from "../stores/userSession";
+
+const userSession = userSessionStore();
 
 
 </script>
